a1eeccafa69ca549ec995332a0c9657f6b44334f
Damien Naudet <naudet@linnaudet.esrf.fr>
2016-06-03 14:36:46 +0200

    Splitting the image to qspace conversion and the fit. Changes to the qspace module.

--- kmap/examples/id01_qspace.py
+++ kmap/examples/id01_qspace.py
@@ -1,5 +1,4 @@
 import os
-#import qspace
 from kmap.process import qspace
 
 import numpy as np
@@ -7,11 +6,8 @@ import numpy as np
 # output directory (some temporary files will also be written there)
 workdir = '/path/to/output/'
 
-# result file
-result_file = '/path/to/results.txt'
-
 # path to the hdf5 "master" file (see id01_spec function)
-master_fn ='/path/to/master.h5'
+master_f ='/path/to/master.h5'
 
 # the beam energy (will take precedence over the one stored in the files, if any)
 beam_energy = 8000.
@@ -34,17 +30,12 @@ qspace.disp_times = True
 # size of the averaging window to use when downsampling
 nav = [4, 4]
 
-res = qspace.img_2_qpeak(master_fn,
-                         workdir,
-                         n_bins,
-                         # beam_energy=beam_energy,
-                         # center_chan=center_chan,
-                         # chan_per_deg=chan_per_deg,
-                         nav=nav,
-                         img_indices=None,
-                         n_threads=None)
-
-with open(result_file, 'w+') as res_f:
-    for i, r in enumerate(res):
-        res_str = 'Img {0} : {1} {2} {3} {4} {5} {6} {7}\n'.format(i, *r)
-        res_f.write(res_str)
+qspace.img_2_qspace(master_f,
+                    workdir,
+                    n_bins,
+                    # beam_energy=beam_energy,
+                    # center_chan=center_chan,
+                    # chan_per_deg=chan_per_deg,
+                    nav=nav,
+                    img_indices=None,
+                    n_proc=None)
--- kmap/process/qspace.py
+++ kmap/process/qspace.py
@@ -52,31 +52,19 @@ img_data_tpl = '/{0}/measurement/image_data/data'
 measurement_tpl = '/{0}/measurement'
 detector_tpl = '/{0}/instrument/image_detector'
 
-# 1d Gaussian func
-gauss_fit = lambda p, x: (p[0] * (1 / np.sqrt(2 * np.pi * (p[2]**2))) *
-                          np.exp(-(x - p[1])**2/(2 * p[2]**2)))
-# 1d Gaussian fit
-e_gauss_fit = lambda p, x, y: (gauss_fit(p, x) - y)
 
-
-def img_2_qpeak(data_h5f,
-                output_dir,
-                n_bins,
-                beam_energy=None,
-                center_chan=None,
-                chan_per_deg=None,
-                nav=(4, 4),
-                img_indices=None,
-                n_threads=None):
+def img_2_qspace(data_h5f,
+                 output_dir,
+                 n_bins,
+                 beam_energy=None,
+                 center_chan=None,
+                 chan_per_deg=None,
+                 nav=(4, 4),
+                 img_indices=None,
+                 n_proc=None):
     """
-    TODO : roi parameter
     TODO : use histogram_lut when available in silx
-    Creates a "master" HDF5 file and one HDF5 per scan. Those scan HDF5 files
-    contain spec data (from *spec_fname*) as well as the associated
-    image data. This file will either contain all valid scans or the one
-    selected using the scan_ids parameter. A valid scan is a scan associated
-    with an (existing) image file. Existing output files will be
-    overwritten.
+    TBD
 
     :param data_h5f: path to the HDF5 file containing the scan counters
         and images
@@ -112,19 +100,21 @@ def img_2_qpeak(data_h5f,
         (TODO : give example)
     :type img_indices: *optional* `array_like`
 
-    :param n_threads: number of threads to use. If None, the number of threads
+    :param n_proc: number of process to use. If None, the number of process
         used will be the one returned by multiprocessing.cpu_count().
-    :type n_threads: `int`
-
-    :returns: a list of tuples (x_pos, y_pos, qx_peak, qy_peak, qz_peak,
-        ||q||, i_peak)
-    :rtype: *list*
+    :type n_proc: `int`
     """
     # TODO : put beam_energy/center_chan at top level or check
     # that all values are identical
     ta = time.time()
 
     base_dir = os.path.dirname(data_h5f)
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    #TODO : overwrite warning
+    output_fn = os.path.join(output_dir, 'qspace.h5')
 
     with h5py.File(data_h5f, 'r') as master_h5:
 
@@ -140,12 +130,6 @@ def img_2_qpeak(data_h5f,
         img_data = master_h5[img_data_tpl.format(entries[0])]
 
         roi = [0, img_data.shape[1], 0, img_data.shape[2]]
-#        if roi is None:
-#            in_roi = [0, img_data.shape[1], 0, img_data.shape[2]]
-#        else:
-#            #TODO : values check
-#            in_roi = roi
-#            img_slice = (slice(roi[0], roi[1]), slice(roi[2], roi[3]))
 
         measurement = master_h5[measurement_tpl.format(entries[0])]
 
@@ -242,8 +226,7 @@ def img_2_qpeak(data_h5f,
                              Nch2=img_y,
                              chpdeg1=chan_per_deg_dim0,
                              chpdeg2=chan_per_deg_dim1,
-                             Nav=nav,
-                             roi=roi)
+                             Nav=nav)
 
         # shape of the array that will store the qx/qy/qz for all
         # rocking angles
@@ -351,6 +334,14 @@ def img_2_qpeak(data_h5f,
         sample_x = measurement['adcX'][:]
         sample_y = measurement['adcY'][:]
 
+    output_shape = (n_images,) + histo.shape
+    _create_result_file(output_fn,
+                        output_shape,
+                        np.float64,
+                        qx_idx,
+                        qy_idx,
+                        qz_idx)
+
     manager = mp.Manager()
 
     # array to store the results
@@ -358,18 +349,21 @@ def img_2_qpeak(data_h5f,
     shared_res = mp_sharedctypes.RawArray(ctypes.c_double, n_xy_pos*5)
 
     entry_locks = [manager.Lock() for n in range(n_entries)]
+    write_lock = manager.Lock()
     idx_queue = manager.Queue()
 
-    if n_threads is None:
-        n_threads = mp.cpu_count()
+    if n_proc is None:
+        n_proc = mp.cpu_count()
 
-    pool = mp.Pool(n_threads,
+    pool = mp.Pool(n_proc,
                    initializer=_init_thread,
                    initargs=(shared_res,
                              idx_queue,
                              entries,
                              entry_files,
+                             output_fn,
                              entry_locks,
+                             write_lock,
                              q_ar,
                              bins_rng,
                              n_bins,
@@ -403,10 +397,11 @@ def img_2_qpeak(data_h5f,
                 self.t_dnsamp = 0.
                 self.t_medfilt = 0.
                 self.t_write = 0.
+                self.t_w_lock = 0.
 
             def update(self, arg):
                 (t_read_, t_lock_, t_dnsamp_, t_medfilt_, t_histo_,
-                 t_mask_, t_fit_, t_write_) = arg
+                 t_mask_, t_fit_, t_write_, t_w_lock_) = arg
                 self.t_histo += t_histo_
                 self.t_fit += t_fit_
                 self.t_mask += t_mask_
@@ -415,15 +410,16 @@ def img_2_qpeak(data_h5f,
                 self.t_dnsamp += t_dnsamp_
                 self.t_medfilt += t_medfilt_
                 self.t_write += t_write_
+                self.t_w_lock += t_w_lock_
         res_times = myTimes()
         callback = res_times.update
     else:
         callback = None
 
     # creating the processes
-    for th_idx in range(n_threads):
+    for th_idx in range(n_proc):
         arg_list = (th_idx,)
-        res = pool.apply_async(_get_q_peak, args=arg_list, callback=callback)
+        res = pool.apply_async(_to_qspace, args=arg_list, callback=callback)
         res_list.append(res)
 
     # sending the image indices
@@ -431,7 +427,7 @@ def img_2_qpeak(data_h5f,
         idx_queue.put(image_idx)
 
     # sending the None value to let the threads know that they should return
-    for th_idx in range(n_threads):
+    for th_idx in range(n_proc):
         idx_queue.put(None)
 
     pool.close()
@@ -455,13 +451,14 @@ def img_2_qpeak(data_h5f,
 
     if(disp_times):
         print('TOTAL {0}'.format(tb - ta))
-        print('Read {0} ({1})'.format(res_times.t_read, res_times.t_lock))
+        print('Read {0} (lock : {1})'.format(res_times.t_read, res_times.t_lock))
         print('Dn Sample {0}'.format(res_times.t_dnsamp))
         print('Medfilt {0}'.format(res_times.t_medfilt))
         print('Histo {0}'.format(res_times.t_histo))
         print('Mask {0}'.format(res_times.t_mask))
         print('Fit {0}'.format(res_times.t_fit))
         print('Write {0}'.format(res_times.t_write))
+        print('(lock : {0})'.format(res_times.t_w_lock))
 
     return final_results
 
@@ -470,7 +467,9 @@ def _init_thread(shared_res_,
                  idx_queue_,
                  entries_,
                  entry_files_,
+                 output_fn_,
                  entry_locks_,
+                 write_lock_,
                  q_ar_,
                  bins_rng_,
                  n_bins_,
@@ -495,7 +494,9 @@ def _init_thread(shared_res_,
             idx_queue,\
             entries,\
             entry_files,\
+            output_fn,\
             entry_locks,\
+            write_lock,\
             q_ar,\
             bins_rng,\
             n_bins,\
@@ -519,7 +520,9 @@ def _init_thread(shared_res_,
         idx_queue = idx_queue_
         entries = entries_
         entry_files = entry_files_
+        output_fn = output_fn_
         entry_locks = entry_locks_
+        write_lock = write_lock_
         q_ar = q_ar_
         bins_rng = bins_rng_
         n_bins = n_bins_
@@ -541,7 +544,20 @@ def _init_thread(shared_res_,
         n_xy_pos = n_xy_pos_
 
 
-def _get_q_peak(th_idx):
+def _create_result_file(h5_fn,
+                        shape,
+                        dtype,
+                        bins_x,
+                        bins_y,
+                        bins_z):
+    with h5py.File(h5_fn, 'w') as h5f:
+        h5f.create_dataset('qspace', shape, dtype=dtype)
+        h5f.create_dataset('bins_edges/x', data=bins_x)
+        h5f.create_dataset('bins_edges/y', data=bins_y)
+        h5f.create_dataset('bins_edges/z', data=bins_z)
+
+
+def _to_qspace(th_idx):
 
     print('Thread {0} started.'.format(th_idx))
 
@@ -553,6 +569,9 @@ def _get_q_peak(th_idx):
     t_dnsamp = 0.
     t_medfilt = 0.
     t_write = 0.
+    t_w_lock = 0.
+    
+    #img = np.ascontiguousarray(np.zeros((516, 516)), dtype=np.float64)
 
     while True:
         image_idx = idx_queue.get()
@@ -560,11 +579,11 @@ def _get_q_peak(th_idx):
             print('Thread {0} is done. Times={1}'
                   ''.format(th_idx, (t_read, t_lock, t_dnsamp,
                                      t_medfilt, t_histo,
-                                     t_mask, t_fit, t_write)))
+                                     t_mask, t_fit, t_write, t_w_lock)))
             if disp_times:
                 return (t_read, t_lock, t_dnsamp,
                         t_medfilt, t_histo,
-                        t_mask, t_fit, t_write,)
+                        t_mask, t_fit, t_write, t_w_lock,)
             else:
                 return None
 
@@ -583,6 +602,9 @@ def _get_q_peak(th_idx):
             try:
                 with h5py.File(entry_files[entry_idx], 'r') as entry_h5:
                     img_data = entry_h5[img_data_tpl.format(entry)]
+                    #img_data.read_direct(img,
+                                         #source_sel=np.s_[image_idx],
+                                         #dest_sel=None)
                     img = img_data[image_idx].astype(np.float64)
             except Exception as ex:
                 print('Error in proc {0} while reading img {1} from entry '
@@ -635,42 +657,16 @@ def _get_q_peak(th_idx):
         cumul[mask] = cumul[mask]/histo[mask]
 
         t_mask += time.time() - t0
-
+        
         t0 = time.time()
+        write_lock.acquire()
+        t_w_lock += time.time() - t0
+        try:
+            with h5py.File(output_fn, 'r+') as output_h5:
+                output_h5['qspace'][image_idx] = cumul
+        except Exception as ex:
+            print('Error in proc {0} while writing result for img {1} : {2}.'
+                  ''.format(th_idx, image_idx, ex))
+        write_lock.release()
 
-        v0 = [1.0, qz.mean(), 1.0]
-        qz_peak = leastsq(e_gauss_fit,
-                          v0[:],
-                          args=(qz_idx, (cumul.sum(axis=0)).sum(axis=0)),
-                          maxfev=100000,
-                          full_output=1)[0][1]
-        v0 = [1.0, qy.mean(), 1.0]
-        qy_peak = leastsq(e_gauss_fit,
-                          v0[:],
-                          args=(qy_idx, (cumul.sum(axis=2)).sum(axis=0)),
-                          maxfev=100000,
-                          full_output=1)[0][1]
-        v0 = [1.0, qx.mean(), 1.0]
-        qx_peak = leastsq(e_gauss_fit,
-                          v0[:],
-                          args=(qx_idx, (cumul.sum(axis=2)).sum(axis=1)),
-                          maxfev=100000,
-                          full_output=1)[0][1]
-        i_peak = leastsq(e_gauss_fit,
-                         v0[:],
-                         args=(qx_idx, (cumul.sum(axis=2)).sum(axis=1)),
-                         maxfev=100000,
-                         full_output=1)[0][0]
-        t_fit += time.time() - t0
-
-        q = np.sqrt(qx_peak**2 + qy_peak**2 + qz_peak**2)
-
-        t0 = time.time()
-        results = np.frombuffer(g_shared_res)
-        results.shape = n_xy_pos, 5
-        results[image_idx] = (qx_peak,
-                              qy_peak,
-                              qz_peak,
-                              q,
-                              i_peak)
         t_write += time.time() - t0

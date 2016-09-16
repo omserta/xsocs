035d336c86e57f731fe6d8c0ffe5bb5f9dcddbd2
Damien Naudet <naudet@linnaudet.esrf.fr>
2016-09-16 12:32:01 +0200

    Missing keyword in example.

--- kmap/examples/id01_merge.py
+++ kmap/examples/id01_merge.py
@@ -1,3 +1,33 @@
+#!/usr/bin/python
+# coding: utf8
+# /*##########################################################################
+#
+# Copyright (c) 2015-2016 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+
+__authors__ = ["D. Naudet"]
+__date__ = "20/04/2016"
+__license__ = "MIT"
+
 import os
 import time
 
@@ -68,6 +98,7 @@ merge_scan_data(output_dir,
                     chan_per_deg=chan_per_deg,
                     pixelsize=pixelsize,
                     center_chan=center_chan,
+                    detector_orient=detector_orient,
                     scan_ids=scan_ids,
                     master_f=master_f,
                     img_dir=img_base,
